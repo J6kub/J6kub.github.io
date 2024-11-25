9c5b52e52086f98a7eb94e115f2b7b3698bf2778
J6kub <jakubb@uia.no>
2024-11-25 15:13:27 +0100

    oops some cookies dropped out of the code

--- sites/is104ow/bambi.js
+++ sites/is104ow/bambi.js
@@ -1,41 +1,28 @@
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
 function switchTheme() {
     var themeLink = document.getElementById("theme-link");
     var currentTheme = themeLink.getAttribute("href");
 
-    if (currentTheme === "style.css") {
-        themeLink.setAttribute("href", "style_dark.css");
-        setCookie("theme", "dark", 7);
+    if (currentTheme.includes("style.css")) {
+        themeLink.setAttribute("href", currentTheme.replace("style.css", "style_dark.css"));
+        localStorage.setItem("theme", "dark");
     } else {
-        themeLink.setAttribute("href", "style.css");
-        setCookie("theme", "light", 7);
+        themeLink.setAttribute("href", currentTheme.replace("style_dark.css", "style.css"));
+        localStorage.setItem("theme", "light");
     }
 }
-function setButtonHeight() {
-    var header = document.querySelector('header');
-    var backButton = document.getElementById('back');
-    backButton.style.height = header.offsetHeight + 'px';
-}
+
+window.onload = function() {
+    var theme = localStorage.getItem("theme");
+    var themeLink = document.getElementById("theme-link");
+    var currentTheme = themeLink.getAttribute("href");
+
+    if (theme === "dark") {
+        themeLink.setAttribute("href", currentTheme.includes("style.css") ? currentTheme.replace("style.css", "style_dark.css") : currentTheme);
+    } else {
+        themeLink.setAttribute("href", currentTheme.includes("style_dark.css") ? currentTheme.replace("style_dark.css", "style.css") : currentTheme);
+    }
+};
+
 
 window.onresize = setButtonHeight;
 
