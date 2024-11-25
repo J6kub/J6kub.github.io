cf42ce365fe17c186446052054dccc7421b50520
J6kub <jakubb@uia.no>
2024-11-25 14:48:46 +0100

    fixed dark theme

---
+++ sites/is104ow/bambi.js
@@ -0,0 +1,43 @@
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function switchTheme() {
+    var themeLink = document.getElementById("theme-link");
+    var currentTheme = themeLink.getAttribute("href");
+
+    if (currentTheme === "style.css") {
+        themeLink.setAttribute("href", "style_dark.css");
+        setCookie("theme", "dark", 7);
+    } else {
+        themeLink.setAttribute("href", "style.css");
+        setCookie("theme", "light", 7);
+    }
+}
+
+window.onload = function() {
+    var theme = getCookie("theme");
+    var themeLink = document.getElementById("theme-link");
+    if (theme === "dark") {
+        themeLink.setAttribute("href", "style_dark.css");
+    } else {
+        themeLink.setAttribute("href", "style.css");
+    }
+};
