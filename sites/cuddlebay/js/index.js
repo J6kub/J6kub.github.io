5afdb2b9eadb2216ee44e95ac9b22df24ff74532
J6kub <jakubb@uia.no>
2025-03-10 19:56:48 +0100

    new shit

--- sites/cuddlebay/js/index.js
+++ sites/cuddlebay/js/index.js
@@ -2,11 +2,35 @@ let pages = document.getElementsByClassName('pages');
 
 function switchTo(toDisp) {
 	Array.from(pages).forEach(function(el) {el.style.display = 'none';})
-	
+	if (toDisp == "main") {
+		navigationDeletation("none");
+		topReplace(false);
+	} else {
+		navigationDeletation("block");
+		topReplace(true);
+	}
 	document.getElementById(toDisp).style.display = "block";
 }
+function navigationDeletation(booly) {
+	let nav = document.querySelector("nav");
+	nav.style.display = booly;
+}
+
+function topReplace(booly) {
+	str = "<text>Trusted In-home Pet Care</text><br><text id='hhi'>Love, safety and a lot of cuddles guaranteed.</text>";
+	toppex = document.getElementById('toppex');
+	
+	if (booly) {
+		toppex.innerHTML = str;
+	} else {
+		toppex.innerHTML = '<text style="font-size:128">Cuddle Bay</text>';
+	}
+}
+
+
+
 
 window.onload = function() {
-	switchTo('abtme');
+	switchTo('main');
 	console.log('potato');
 }
