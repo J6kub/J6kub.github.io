6a2525a7d094bd83ad79a9fe2c505e729e5486e7
J6kub <jakubb@uia.no>
2025-03-15 20:49:20 +0100

    fixed lowres

--- sites/cuddlebay2/script.js
+++ sites/cuddlebay2/script.js
@@ -1,3 +1,4 @@
+let foot = document.querySelector('footer');
 document.addEventListener("DOMContentLoaded", function () {
     let slides = document.querySelectorAll(".slide");
     let slideContainer = document.querySelector(".slideshow");
@@ -10,3 +11,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
     setInterval(nextSlide, 3000);
 });
+
+function AdjustStyleSheet() {
+    let styleEl = document.getElementById('variable_style');
+		if (window.innerWidth < window.innerHeight) {
+		// Phone
+	} else if (window.innerHeight < 720) {
+		styleEl.href = "styles_lowres.css";
+	} else {
+		styleEl.href = "styles_lowres.css";
+	}
+	relocateFooter();
+}
+
+function relocateFooter() {
+	
+	foot.style.top = getDocumentHeight() - (foot.clientHeight*1.5) + "px";
+}
+
+
+
+window.onresize = function() {
+	AdjustStyleSheet();
+}
+window.onload = function() {
+	AdjustStyleSheet();
+	relocateFooter();
+
+}
+foot.onload = setTimeout(function() {relocateFooter()},100);
+
+
+function getDocumentHeight() {
+    return Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.body.clientHeight,
+        document.documentElement.clientHeight
+    );
+}
